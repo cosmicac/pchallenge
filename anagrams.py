35ca8ff2b2ab3b0ddbbb7e08bca5398444182fb7
Tom Li <weirdotomli@gmail.com>
2016-12-01 15:33:12 -0800

    almost forgot sorting

--- anagrams.py
+++ anagrams.py
@@ -5,8 +5,6 @@ import sys
 # find all anagrams of w1 in word_bank 
 # by creating character count dictionaries 
 # and checking if dictionaries are equal
-# should take O(n + m) time, where n is the length of 
-# the argument word, m is the total length of our word bank
 def find_anagrams(w1, word_bank):
 
 	# create dictionary of character counts
@@ -16,6 +14,7 @@ def find_anagrams(w1, word_bank):
 	# character count dictionaries are equal
 	return [w2 for w2 in word_bank if char_counts_w1 == make_char_counts(w2.lower())]
 			
+# make a character count dictionary out of input word
 def make_char_counts(w):
 	char_counts = defaultdict(int)
 	for c in w:
@@ -27,13 +26,23 @@ if __name__ == '__main__':
 	# path to the txt file from arguments
 	path_to_txt = sys.argv[1]
 
-	# extract lines into a list
+	# extract lines into a word bank
 	word_bank = open(path_to_txt).read().split('\n')
+
+	# input loop
 	while True:
+
+		#get input word to match
 		word_to_match = raw_input() 
+
+		# if input is empty string, just exit
 		if word_to_match == '':
 			break
-		anagrams = find_anagrams(word_to_match, word_bank)
+
+		# find anagrams of input word in word bank
+		anagrams = sorted(find_anagrams(word_to_match, word_bank))
+
+		# output matches if any
 		if anagrams:
 			for a in anagrams:
 				print(a, end=" ")
